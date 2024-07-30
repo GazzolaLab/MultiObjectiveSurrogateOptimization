75d4c23a7b334270d3e2968a09dfd7ab805f626d
Frithjof Gressmann <frithjof@noc.io>
2024-07-29 19:58:32 -0500

    Include cross-check label

--- interface/sopt.py
+++ interface/sopt.py
@@ -7,7 +7,9 @@ class Sopt(Dmosopt):
 
     @property
     def custom_training(self) -> bool:
-        return self.config.dopt_params.get("surrogate_custom_training", None) is not None
+        return (
+            self.config.dopt_params.get("surrogate_custom_training", None) is not None
+        )
 
     @property
     def custom_training_kwargs(self) -> dict:
@@ -18,11 +20,11 @@ class Sopt(Dmosopt):
 
     @property
     def custom_surrogate_name(self) -> str:
-        return "joint-" + self.custom_training_kwargs.get("mode", 'c+o')
+        return "joint-" + self.custom_training_kwargs.get("mode", "c+o")
 
     @property
     def surrogate_method_name(self) -> str:
-        if self.custom_training and self.custom_training_kwargs.get('objectives', True):
+        if self.custom_training and self.custom_training_kwargs.get("objectives", True):
             return self.custom_surrogate_name
 
         return super().surrogate_method_name
@@ -33,7 +35,7 @@ class Sopt(Dmosopt):
 
     @property
     def mC(self) -> str:
-        if self.custom_training_kwargs.get('constraints', False):
+        if self.custom_training_kwargs.get("constraints", False):
             return self.custom_surrogate_name
 
         if self.config.dopt_params.get("feasiblity", None) is True:
@@ -43,8 +45,8 @@ class Sopt(Dmosopt):
 
     @property
     def mS(self):
-        if self.custom_training_kwargs.get('sensitivity', False):
-            return self.custom_surrogate_name
+        if s := self.custom_training_kwargs.get("sensitivity", False):
+            return self.custom_surrogate_name + (f"-{s}" if isinstance(s, str) else "")
 
         if (
             name := self.config.dopt_params.get("sensitivity_method_name", None)
@@ -153,21 +155,18 @@ class Sopt(Dmosopt):
         return _Wrapper(name, self.xlb, self.xub)
 
     def label(self):
-        m = self.config.dopt_params.opt_id.replace('dmosopt_', '') + "::" + self.m
+        m = self.config.dopt_params.opt_id.replace("dmosopt_", "") + "::" + self.m
 
         if self.config.dopt_params.get("surrogate_custom_training", None) is None:
             return m
-        
-        fs = self.custom_training_kwargs.get('feasibility_solving', False)
-        fs = 'fs' if fs else '-'
-        
+
+        fs = self.custom_training_kwargs.get("feasibility_solving", False)
+        fs = "fs" if fs else "-"
+
         return m + "[" + fs + "]"
 
-    def version_joint_model(
-        self,
-        **kwargs
-    ):
-        if kwargs.get('mode', 'c+o') not in ['c+o', 'c', 'o']:
+    def version_joint_model(self, **kwargs):
+        if kwargs.get("mode", "c+o") not in ["c+o", "c", "o"]:
             raise ValueError("Invalid mode")
         return {
             "dopt_params": {
@@ -176,10 +175,7 @@ class Sopt(Dmosopt):
             }
         }
 
-    def version_dynamic_sampling(
-        self,
-        **kwargs
-    ):
+    def version_dynamic_sampling(self, **kwargs):
         return {
             "dopt_params": {
                 "dynamic_initial_sampling": "models.ops.dynamic_sampling",
