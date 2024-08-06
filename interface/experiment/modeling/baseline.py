8a8ec9d4392653a627314891470a3b19334480d2
Frithjof Gressmann <frithjof@noc.io>
2024-08-06 11:52:20 -0500

    Update baseline scope

--- interface/experiment/modeling/baseline.py
+++ interface/experiment/modeling/baseline.py
@@ -1,11 +1,11 @@
-from machinable import Interface, get, Execution
+from machinable import Interface, get
 
 
 class Baseline(Interface):
 
     def launch(self):
         for trial in range(1):
-            with get("machinable.scope", {"trial": trial, 'cohort': 'baseline1'}):
+            with get("machinable.scope", {"trial": trial}):
                 for nm in [
                     "SCA",
                     "IVY",
@@ -13,23 +13,25 @@ class Baseline(Interface):
                     "CCKBC",
                     "AAC",
                     "BS",
-                    "OLM", 
+                    "OLM",
                     "NGFC",
-                    "IS"
+                    "IS",
                 ]:
-                    protocol = [
+                    get(
                         "interface.sopt_modeling",
                         f"""~from_protocol("benchmarks/ca1_pinsky_rinzel_modeling/config/CA1_{nm}.yaml")""",
-                    ]
-
-                    # no surrogate
-                    get(protocol, {
-                        'nodes': "128",
-                    }).launch()
+                    ).launch()
 
     def progress(self):
         for c in self.components:
             print(c.label())
-            done = len(c.load_h5()['epochs'])
-            print(done, ' / ', c.num_evals_total, ' (', round(done / c.num_evals_total * 100), '%)')
-            print('')
+            done = len(c.load_h5()["epochs"])
+            print(
+                done,
+                " / ",
+                c.num_evals_total,
+                " (",
+                round(done / c.num_evals_total * 100),
+                "%)",
+            )
+            print("")
