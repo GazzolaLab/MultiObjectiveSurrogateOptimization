4c0fe1cf69c359ce33bbd63e3d0b83de77bc2f7c
Frithjof Gressmann <frithjof@noc.io>
2025-10-22 12:49:16 -0500

    Remote upload weights

--- models/worker.py
+++ models/worker.py
@@ -29,7 +29,21 @@ while True:
         model.autofit(x, y, yC, verbose=1, epochs=epochs)
         scores = model.autoeval(x, y, yC)
 
-        model.save_weights(os.path.join(queue, f"current.weights.h5"))
+        weights_filename = "current.weights.h5"
+
+        if q.is_remote:
+            local_weights_path = os.path.join(q.local_temp_dir, weights_filename)
+            model.save_weights(local_weights_path)
+
+            q._ensure_remote_dir()
+            remote_weights_path = os.path.join(q.remote_path, weights_filename).replace(
+                "\\",
+                "/",
+            )
+            q._scp_to_remote(local_weights_path, remote_weights_path)
+            os.remove(local_weights_path)
+        else:
+            model.save_weights(os.path.join(queue, weights_filename))
 
     print(f"Tasks {i} finished, sending data")
 
